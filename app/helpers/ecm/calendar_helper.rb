d99f375989156488d7a4d47ced61c3b5af8a9c93
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-03-07 23:38:49 +0100

    Complete rework. Swith to flexbox layout. Switch to haml partials for rendering. Cleanup helpers.

--- app/helpers/ecm/calendar_helper.rb
+++ app/helpers/ecm/calendar_helper.rb
@@ -2,15 +2,17 @@ module Ecm
   module CalendarHelper
     # renders a calendar table
     def month_calendar(date = Time.zone.now.to_date, elements = [], options = {})
-      # default options
       options.reverse_merge! :date_method => :start_at, :display_method => :to_s, :link_elements => true, :start_day => :sunday
+
+      display_method = options.delete(:display_method)
+      link_elements  = options.delete(:link_elements)
       
       # calculate beginning and end of month
       beginning_of_month = date.beginning_of_month.to_date
       end_of_month = date.end_of_month.to_date
 
       # Get localized day names
-      localized_day_names = I18n.t('date.day_names').dup
+      localized_day_names = I18n.t('date.abbr_day_names').dup
 
       # Shift day names to suite start day
       english_day_names = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
@@ -20,75 +22,51 @@ module Ecm
 
       # Get the offset of start day
       offset = english_day_names.index(options[:start_day])
+      last_day_of_week =  Time.zone.now.end_of_week.wday
 
 
       # Change calendar heading if offset is not 0 (offset 0 means sunday is the first day of the week)
       offset.times do 
         localized_day_names.push(localized_day_names.shift)
       end
-    
-      content_tag(:table, :class => 'calendar') do
-        table = ''
+
+      days_by_week = {}
+      days_in_week = {}
+      week = beginning_of_month.cweek
+      (beginning_of_month..end_of_month).each_with_index do |day, index|
+        days_in_week[day] = elements.find_all { |e| e.send(options[:date_method]).to_date == day.to_date } || {}
         
-        table << content_tag(:tr,  localized_day_names.collect { |day| content_tag(:th, day) }.join("").html_safe)
-        table << content_tag(:tr) do
-          table_content = ''
-          
-          (beginning_of_month..end_of_month).each do |d|
-            if d == beginning_of_month
-              column_offset = d.wday - offset
-              column_offset += 7 if column_offset < 0
-              column_offset.times do
-                table_content << content_tag(:td, nil, :class => 'offset')
-              end
-            end
-             
-            if d.wday == (0 + offset)
-               table_content << "</tr><tr>"
-            end
-             
-            # Create content for day
-            day_content = "#{d.day}<br/ >".html_safe
-            elements_for_day = elements.find_all { |e| e.send(options[:date_method]).to_date == d.to_date }
-            if options[:link_elements]
-              elements_for_day.collect! { |e| content_tag(:div, link_to(e.send(options[:display_method]), e), :class => 'calendar_entry') }
-            else
-              elements_for_day.collect! { |e| content_tag(:div, e.send(options[:display_method]), :class => 'calendar_entry') }
-            end
-            
-            day_content << elements_for_day.join("").html_safe
-            
-            # css classes for day td
-            if d == Time.zone.now.to_date
-              day_css_classes = 'calendar_day today'
-            else
-              day_css_classes = 'calendar_day'
-            end 
-            
-            table_content << content_tag(:td, day_content, :class => day_css_classes)
+        if day.wday == last_day_of_week
+          (7 - days_in_week.size).times do |i|
+            days_in_week[0 - i] = {}
           end
-          
-          table_content.html_safe
+          week = (day + 1.day).cweek
         end
-        
-        table.html_safe
+
+        if day.wday == last_day_of_week || index == end_of_month.day - 1
+          days_by_week[week] = days_in_week 
+          days_in_week = {}
+        end
+
+        if index == end_of_month.day - 1
+          (7 - days_in_week.size).times do |i|
+            days_in_week[days_in_week.size - i] = {}
+          end
+        end
       end
+      render partial: 'ecm/calendar_helper/month_calendar', locals: { localized_day_names: localized_day_names, days_by_week: days_by_week, display_method: display_method, link_elements: link_elements }
     end
     
-    def month_calendar_pagination(date)
-      actual_month = Time.zone.now.beginning_of_month.to_date
-   
+    def month_calendar_pagination(date, options = {})
+      options.reverse_merge!(show_today_link: true)
+
+      show_today_link = options.delete(:show_today_link)
+
+      actual_month   = Time.zone.now.beginning_of_month.to_date
       previous_month = date.beginning_of_month.to_date - 1.month
-      next_month = date.beginning_of_month.to_date + 1.months      
-      
-      content_tag(:div, :class => 'calendar_pagination') do
-        [
-          link_to('Heute', url_for(:month => actual_month.month, :year => actual_month.year)),
-          link_to("<", url_for(:month => previous_month.month, :year => previous_month.year)),
-          link_to(">", url_for(:month => next_month.month, :year => next_month.year)),
-          I18n.l(date, :format => :month_with_year),
-        ].join(" | ").html_safe
-      end
+      next_month     = date.beginning_of_month.to_date + 1.months
+
+      render partial: 'ecm/calendar_helper/month_calendar_pagination', locals: { actual_month: actual_month, previous_month: previous_month, next_month: next_month, show_today_link: show_today_link, date: date }
     end
   end
 end  
